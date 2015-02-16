17f71b7d1a69dd4dea29220035f075a6ee2b533d
Douglas Miller <dougmiller@themetacity.com>
2015-02-17 00:19:20 +1000

    One button in place

--- FileRunner.ino
+++ FileRunner.ino
@@ -1,16 +1,51 @@
 int greenLED = 0;
 int redLED = 1;
 
-void setup() {                
-  pinMode(greenLED, OUTPUT);     
-  pinMode(redLED, OUTPUT);     
+const int redButton1 = 5;
+const int redButton2 = 3;
+const int blackButton1 = 4;
+const int blackButton2 = 6;
+const int safetyToggle = 2;
+
+const int pins[] = {2,3,4,5,6};
+
+int buttonState = HIGH;
+int lastButtonState = HIGH;
+
+void setup() {
+    pinMode(greenLED, OUTPUT);
+    pinMode(redLED, OUTPUT);
+    digitalWrite(greenLED, LOW);
+    digitalWrite(redLED, LOW);
+
+    pinMode(redButton1, INPUT_PULLUP);
+    pinMode(redButton2, INPUT_PULLUP);
+    pinMode(blackButton1, INPUT_PULLUP);
+    pinMode(blackButton2, INPUT_PULLUP);
+    pinMode(safetyToggle, INPUT_PULLUP);
+
+    Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(greenLED, HIGH);
-  digitalWrite(redLED, LOW);
-  delay(1000);
-  digitalWrite(greenLED, LOW);
-  digitalWrite(redLED, HIGH);
-  delay(1000);
+    buttonState = digitalRead(redButton1);
+    
+    if (buttonState != lastButtonState) {
+         if (buttonState == LOW) {
+           Serial.println("redButton1");
+           digitalWrite(greenLED, HIGH);
+           digitalWrite(redLED, HIGH);
+           delay(50);
+           digitalWrite(greenLED, LOW);
+           digitalWrite(redLED, LOW);
+           delay(50);
+           digitalWrite(greenLED, HIGH);
+           digitalWrite(redLED, HIGH);
+           delay(50);
+           digitalWrite(greenLED, LOW);
+           digitalWrite(redLED, LOW);
+       }
+    }
+    lastButtonState = buttonState;
 }
+
